f1f709be3453894520b379be7f94d262b853954e
潘语笛 <panyudi.sigoto@bytedance.com>
2025-12-15 10:44:09 +0800

    Initial Commit

---
+++ YDCutterTests/YDCutterTests.swift
@@ -0,0 +1,17 @@
+//
+//  YDCutterTests.swift
+//  YDCutterTests
+//
+//  Created by PanYuDi on 2025/12/15.
+//
+
+import Testing
+@testable import YDCutter
+
+struct YDCutterTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
